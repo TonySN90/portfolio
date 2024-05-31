636de27a3642bb04c26fc914aff0163115948b8b
TonySN90 <tony.heider90@gmail.com>
2024-05-31 23:52:49 +0200

    add Github Contribution

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -2,6 +2,7 @@ import { FaCode } from "react-icons/fa";
 import SectionWrapper from "../components/SectionWrapper";
 import Title from "../components/Title";
 import SubTitle from "../components/SubTitle";
+import { LuGithub } from "react-icons/lu";
 
 function About() {
   return (
@@ -12,7 +13,7 @@ function About() {
     >
       <SectionWrapper>
         <Title>WerBinIch</Title>
-        <div className="z-30 bg-transparent flex flex-col sm:flex-row gap-6">
+        <div className="z-30 bg-transparent flex flex-col sm:flex-row gap-6 mb-4">
           <div className="z-30 flex flex-col gap-2 w-full md:w-[60%] uppercase bg-transparent pointer-events-none text-left">
             <p className="bg-transparent">
               Geboren in Bergen und aufgewachsen auf Rügen. Inzwischen
@@ -34,9 +35,7 @@ function About() {
               <div className="bg-color_primary min-w-8 size-8 flex justify-center items-center rounded-full">
                 <FaCode className="bg-transparent text-color_background size-4" />
               </div>
-              <span className="bg-transparent">
-                Tech Stack // Erfahrungen mit...
-              </span>
+              <p className="bg-transparent">Tech Stack // Erfahrungen mit...</p>
             </SubTitle>
             <div className="flex flex-wrap gap-2 bg-transparent">
               <Chip>Javascript</Chip>
@@ -51,6 +50,24 @@ function About() {
               <Chip>Supabase</Chip>
             </div>
           </div>
+        </div>
+        <div className="z-30 bg-transparent w-[100%]">
+          <SubTitle>
+            <div className="bg-color_primary min-w-8 size-8 flex justify-center items-center rounded-full">
+              <LuGithub className="bg-transparent text-color_background size-5" />
+            </div>
+            <p className="bg-transparent">Contributions</p>
+          </SubTitle>
+          <a
+            className="z-30 bg-transparent p-4 rounded-lg"
+            href="https://github.com/TonySN90"
+          >
+            <img
+              className="w-full"
+              src="https://ghchart.rshah.org/TonySN90"
+              alt="GitHub Contributions"
+            />
+          </a>
         </div>
       </SectionWrapper>
     </section>
