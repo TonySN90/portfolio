de14325a6bf68b39cf13bf983e9b228c2f578b01
TonySN90 <tony.heider90@gmail.com>
2024-06-02 00:18:18 +0200

    add contact

--- src/Contact/Contact.tsx
+++ src/Contact/Contact.tsx
@@ -1,15 +1,38 @@
+import { MdEmail } from "react-icons/md";
 import SectionWrapper from "../components/SectionWrapper";
 import Title from "../components/Title";
+import { FiGithub } from "react-icons/fi";
+import { FaLinkedinIn } from "react-icons/fa";
 
 function Contact() {
   return (
     <section
       id="kont"
-      className="relative flex justify-center items-center py-16
-    snap-start text-right sm:text-left w-[calc(100vw-3rem)] sm:w-[calc(100vw-4rem)]"
+      className="relative h-[50vh] flex justify-center items-center py-16
+    snap-start text-center w-[calc(100vw-3rem)] sm:w-[calc(100vw-4rem)]"
     >
       <SectionWrapper>
-        <Title>Kontakt</Title>
+        <Title center={true}>Kontakt</Title>
+        <p className="m-auto z-30 mb-8 w-[90%]">
+          Lust auf Kontakt? Dann schick mir einfach eine E-Mail. Ich bin offen
+          für Ideen, Vorschläge und Anregungen. <br /> Let&#x27;s connect!
+        </p>
+        <div className="m-auto flex items-center gap-2 z-30 bg-transparent mb-4">
+          <MdEmail className="text-2xl" />
+          <p className="text-xl sm:text-2xl font-bold">
+            tony.heider90@gmail.com
+          </p>
+        </div>
+        <p className="z-30 bg-transparent mb-6">Du findest mich auch auf...</p>
+        <div className="m-auto z-30 flex gap-5 bg-transparent">
+          <div className="size-14 bg-rose-600 flex justify-center items-center rounded-full cursor-pointer">
+            <FiGithub className="size-6 bg-transparent " />
+          </div>
+          <div className="size-14 bg-rose-600 flex justify-center items-center rounded-full cursor-pointer">
+            <FaLinkedinIn className="size-6 bg-transparent" />
+          </div>
+        </div>
+        <div className="h-[200px]"></div>
       </SectionWrapper>
     </section>
   );
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,19 +1,23 @@
 function Title({
   children,
   reverse,
+  center,
   size,
 }: {
   children: React.ReactNode;
   reverse?: boolean;
+  center?: boolean;
   size?: number;
 }) {
   return (
     <div
       style={{
         flexDirection: reverse ? "row-reverse" : "row",
+        textAlign: center ? "center" : "left",
       }}
       className="flex w-full items-center gap-6 bg-transparent"
     >
+      {center && <div className="w-[100%] h-[1px] bg-stone-600 z-30"></div>}
       <h2
         style={{ fontSize: `${size}rem` }}
         className="flex text-[2.4rem] sm:text-[3rem] font-bold z-30 bg-transparent"
